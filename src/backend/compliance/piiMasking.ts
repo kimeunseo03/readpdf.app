479cbec5c8344378e7a835faae81838a36b5206b
kimeunseo03 <dmstj4677@gmail.com>
2026-05-13 01:59:38 +0900

    Delete src directory

--- src/backend/compliance/piiMasking.ts
+++
@@ -1,8 +0,0 @@
-export function maskSensitiveText(input: string): string {
-  return input
-    .replace(/\d{6}\s*-\s*\d{7}/g, "******-*******")
-    .replace(/([가-힣]{1})([가-힣])([가-힣]?)(?=\s*(?:소유자|권리자|채무자|근저당권자)?)/g, (_m, a, _b, c) => {
-      if (!c) return `${a}*`;
-      return `${a}*${c}`;
-    });
-}
